3d24e49a3fad9f18af60fc52263ac9b61e368a38
Bosch Leith <bosch.leith@gmail.com>
2022-01-06 15:15:33 -0600

    Added: @Autowired functionality

--- src/main/java/com/example/springautowired/Heart.java
+++ src/main/java/com/example/springautowired/Heart.java
@@ -2,6 +2,25 @@ package com.example.springautowired;
 
 public class Heart {
 
+	private String typeOfHeart;
+	private int numOfHeart;
+
+	public String getTypeOfHeart() {
+		return typeOfHeart;
+	}
+
+	public void setTypeOfHeart(String typeOfHeart) {
+		this.typeOfHeart = typeOfHeart;
+	}
+
+	public int getNumOfHeart() {
+		return numOfHeart;
+	}
+
+	public void setNumOfHeart(int numOfHeart) {
+		this.numOfHeart = numOfHeart;
+	}
+
 	public void pump(){
 		System.out.println("Your heart is pumping");
 		System.out.println("Alive");
--- src/main/java/com/example/springautowired/Human.java
+++ src/main/java/com/example/springautowired/Human.java
@@ -1,15 +1,27 @@
 package com.example.springautowired;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Human {
 
+	@Autowired
+	@Qualifier("humanHeart")
 	private Heart heart;
+
+	public Human() {
+	}
+
+	public Human(Heart heart) {
+		this.heart = heart;
+	}
 
 	public void setHeart(Heart heart) {
 		this.heart = heart;
 	}
 
-	public void startPumping(){
-		if (heart != null){
+	public void startPumping() {
+		if (heart != null) {
 			heart.pump();
 		} else {
 			System.out.println("You are dead!");
